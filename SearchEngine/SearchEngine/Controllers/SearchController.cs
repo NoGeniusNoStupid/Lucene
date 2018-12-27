ae4f0cc62aace0f0a52ac800b3a8a37908e76519
jhj <651499253@qq.com>
2018-12-27 11:38:47 +0800

    项目创建

---
+++ SearchEngine/SearchEngine/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchEngine.Controllers
+{
+    //搜索类
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
